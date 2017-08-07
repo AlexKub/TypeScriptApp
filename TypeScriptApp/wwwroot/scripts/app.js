8bba135e0f4c49db50557068124f61df1a20850b
Cocon <Super@mail.ru>
2017-08-07 03:53:16 +0300

    Add project files.

---
+++ TypeScriptApp/wwwroot/scripts/app.js
@@ -0,0 +1,6 @@
+function sayHello() {
+    var compiler = document.getElementById("compiler").value;
+    var framework = document.getElementById("framework").value;
+    return "Hello from nov TOURee";
+}
+//# sourceMappingURL=app.js.map
---
+++ TypeScriptApp/wwwroot/scripts/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var browser_1 = require("angular2/platform/browser");
+var thirdtask_1 = require("./thirdtask");
+browser_1.bootstrap(thirdtask_1.MyApp);
+//# sourceMappingURL=main.js.map
